73056d07140d8ec34946dc7fab5738dbf957ba80
root <imanikibona07@gmail.com>
2023-09-10 09:09:57 -0700

    task four_using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
-/*
- * main is the entry point
- * return always succeed
-*/
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
