1822c3604c73d0c5fbdc7d404588edd95ca11dab
root <imanikibona07@gmail.com>
2023-09-14 02:29:05 -0700

    task nine_Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int z;
+
+	z = 1;
+	printf("%d", z);
+	for (z = 2; z <= 100; z++)
+	{
+		if ((z % 3 == 0) && (z % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (z % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (z % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", z);
+		}
+	}
+	printf("\n");
+	return (0);
+}
