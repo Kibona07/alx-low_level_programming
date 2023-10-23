f3eb6c8fc8cbe72ee405f3030aa5f794cc0a0151
Kibona07 <imanikibona07@gmail.com>
2023-10-23 00:21:12 -0700

    task two

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int m;
+
+    m = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", m);
+    return (0);
+}
