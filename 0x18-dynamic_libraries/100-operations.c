b5ee51e47d5551ebb6eecedbe7b997a386f2d79e
kibona07 <imanikibona07@gmail.com>
2023-12-15 12:56:00 -0800

    missing C when coding in Python. So let’s fix that

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * _add - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int _add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * _sub - substration
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int _sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * _mul - multiplication
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+
+int _mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
