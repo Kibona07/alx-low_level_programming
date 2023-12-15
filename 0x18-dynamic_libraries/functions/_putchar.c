fa38fe62471cf8ab46af3c1c54fef24a99e3d904
kibona07 <imanikibona07@gmail.com>
2023-12-15 12:14:08 -0800

    dynamic library created

---
+++ 0x18-dynamic_libraries/functions/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - program function
+ * Return: zero on success
+ * @c: function parameter
+ */
+
+int _putchar(char c)
+{
+	_putchar(c);
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/functions/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print to std output
+ *
+ * Return: return nothing
+ */
+
+int main(void)
+{
+	_puts("not bad am still here\n");
+	return (0);
+}
